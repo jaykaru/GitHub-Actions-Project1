36708c2cb4600722c0e68f87240bb62a72e5053b
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-30 21:01:32 +0000

    create a sunbet resource

--- main.tf
+++ main.tf
@@ -8,3 +8,15 @@ resource "aws_vpc" "myvpc" {
     Name = "myvpc"
   }
 }
+
+# Create a subnet sub1
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sub1-10.0.1.0/24-eu-west-2a"
+  }
+}
