2aee03f843440e2ceff0aa7ad0e5af3bbc1efd2e
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-30 21:04:32 +0000

    create RT

--- main.tf
+++ main.tf
@@ -29,3 +29,17 @@ resource "aws_internet_gateway" "igw" {
     Name = "main-igw"
   }
 }
+
+# Create a Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
